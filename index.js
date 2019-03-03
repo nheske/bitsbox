8820612a6d77749299a9428105837236fe5e4129
nheske <32206709+nheske@users.noreply.github.com>
2019-03-03 06:29:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-deck = [];
-suit = [];
-rank = [];
-players = [];
-nextCard = 0;
-isTableCheck = false;
+var deck = [];
+var c6, c7;
+var suit = [];
+var rank = [];
+var players = [];
+var nextCard = 0;
+var isTableCheck = false;
 var lastPingRequestTime;
-debug = text('debug',100,900)
-playersLabel = text('',100,1000,50)
-var id=Date.now().toString().substring(9,12);
-players[0] = id;
-updatePlayersLabel();
+var playersLabel = text('',100,1000,50)
+var self=Date.now().toString().substring(9,12);
+var debug = text(self,100,900);
+var message = text('',40, 800, 40);
 board();
 reset();
-//var id = window.navigator.userAgent.replace(/\D+/g, '').substring(0,3);
-debug.change(id);
 
-seconds = 600;
-words = text('GO', 100, 100, 100, 'black');
-clock = stamp('clock',600,50, 50);
-clock.tap = resetTimer;
-pause = false;
-pauseButton = stamp('pawn', 700, 50, 80);
-pauseButton.tap = pauseClock;
+var seconds = 600;
+var words = text('GO', 100, 100, 100, 'black');
+var isPaused = false;
+
+function showCards(){
+  send(['show', self, c6, c7]);
+}
+
+function clearTable(){
+  send(['clear']);
+  get(['clear']);
+}
 
 function pauseClock(){
-  pause = !pause;
-  pauseList = ['pause', pause];
-  send(pauseList);
+  isPaused = !isPaused;
+  send(['pause', isPaused]);
 }
 
 function resetTimer(){
-  seconds = 600;
-  console.log('seconds='+seconds);
-  pingList = [];
-  pingList[0] = 'resetTimer';
-  send(pingList);
-  pause = false;
+  send(['resetTimer']);
+  get(['resetTimer']);
 }
 
 function loop(){
-  if(!pause){
+  if(!isPaused){
     if(seconds > 0) {
      seconds = seconds -0.05;
-     minutes = Math.floor(seconds/60);
+     var minutes = Math.floor(seconds/60);
      var displaySeconds = Math.round(seconds % 60);
      words.change(minutes+':'+displaySeconds);
    } else{
@@ -54,32 +52,50 @@ function loop(){
 }
 
 function preflop(){
-  for (i = 0; i < players.length; i++) {
-    someId = players[i];
-    card1 = getCard();
-  	card2 = getCard();
-    list = [];
-    list[0] = 'deal'
-    list[1] = card1;
-    list[2] = card2;
+  for (var i = 0; i < players.length; i++) {
+    var someId = players[i];
+    var card1 = getCard();
+  	var card2 = getCard();
+    var packets = [];
+    packets[0] = 'deal'
+    packets[1] = card1;
+    packets[2] = card2;
     console.log('dealing '+card1+','+card2);
-    if(someId == id){
+    if(someId == self){
       console.log('my cards');
-	  renderDeal(card1, card2);
+//	  renderDeal(card1, card2);
+      get(packets);
     } else{
-      send(list);
+      send(packets);
     }
   }
   f.change('flop');
   f.tap = flop;
 }
 
-function renderDeal(){
+function renderDeal(card1, card2){
       c6.change(card1);
       c7.change(card2);
 }
 
-function updatePlayersLabel(){
+function removePlayer(player){
+  temp = players[players.length]
+  for (i = 0; i < players.length; i++) {
+    if(players[i] == player){
+        players[i] = temp;
+        players.pop();
+        console.log(player+' removed');
+      }
+    }
+  label = 'players: '
+  for (i = 0; i < players.length; i++) {
+    label += ' '+players[i];
+  }
+  playersLabel.change(label);
+}
+
+function addPlayer(player){
+  players.push(player);
   label = 'players: '
   for (i = 0; i < players.length; i++) {
     label += ' '+players[i];
@@ -94,114 +110,95 @@ function endTableCheck(){
 function tableCheck() {
   isTableCheck = false;
   players = [];
-  players.push(id);
+  players.push(self);
   lastPingRequestTime = Math.floor(Date.now() / 1000)
-  pingRequestList = [];
-  pingRequestList[0] = 'ping_request';
-  pingRequestList[1] = lastPingRequestTime;
-  pingRequestList[2] = id;
-  send(pingRequestList);
+  send(['ping_request',lastPingRequestTime,self]);
   delay(endTableCheck, 2000);
 }
 
 function ping() {
   time = Math.floor(Date.now()/1000)
-  pingList = [];
-  pingList[0] = 'ping';
-  pingList[1] = time;
-  pingList[2] = id;
-  send(pingList);
+  send(['ping',time,self]);
 }
 
 // initial call, or just call refresh directly
 //setTimeout(ping, 5000);
 
 function getCard(){
-  //var randomnumber = Math.floor(Math.random()*deck.length);
-  //var randomnumber = random(deck.length);
-  //var someCard = deck[randomnumber];
   var someCard = deck[nextCard];
   nextCard = nextCard + 1;
-  console.log('removing '+someCard)
-  //deck[randomnumber] = deck[deck.length-1]
-  //deck.pop();
-  console.log('getCard='+someCard);
   return someCard
 }
 
 function flop(){
-  card1 = getCard();
-  card2 = getCard();
-  card3 = getCard();
-  cards = [card1,card2,card3]
-//  debug.change('flop a='+card1+' b='+card2+' c='+card3);
-  list = [];
-  list[0] = 'flop'
-  list[1] = card1;
-  list[2] = card2;
-  list[3] = card3;
-  send(list);
-  renderFlop(card1, card2, card3);
+//  var card1 = getCard();
+//  card2 = getCard();
+//  card3 = getCard();
+//  cards = [card1,card2,card3]
+  var packets = [];
+  packets[0] = 'flop'
+  packets[1] = getCard();
+  packets[2] = getCard();
+  packets[3] = getCard();
+  send(packets);
+  get(packets);
+  //renderFlop(card1, card2, card3);
 }
 
-//function get(action, card1, card2, card3) {
-function get(list) {
-  action = list[0];
-  card1 = list[1];
-  card2 = list[2];
-  card3 = list[3];
-//  console.log('in get '+action+' id='+id+' card1='+card1+' card2='+card2+' card3='+card3)
+function get(input) {
+  action = input[0];
+  arg1 = input[1];
+  card2 = input[2];
+  card3 = input[3];
+ // console.log('in get '+action+' id='+id+' card1='+card1+' card2='+card2+' card3='+card3)
 //  debug.change('in get '+action+' id='+id+' card1='+card1+' card2='+card2+' card3='+card3)
   if(action == 'flop'){
-    renderFlop(card1, card2, card3)
+    renderFlop(arg1, card2, card3)
   } else if(action == 'turn'){
-    renderTurn(card1)
+    renderTurn(arg1)
   } else if(action == 'river'){
-    renderRiver(card1)
+    renderRiver(arg1)
   } else if(action == 'shuffle'){
     reset();
   } else if(action == 'joined'){
-    debug.change(card1+' just joined')
+    debug.change(arg1+' just joined')
   } else if(action == 'ping'){
-    someId = list[2];
+    someId = input[2];
     console.log('got ping '+someId);
     if(!players.includes(someId)){
-      players.push(someId);
-      updatePlayersLabel();
+      addPlayer(someId);
     }
   } else if(action == 'ping_request'){
   //  ping();
   } else if(action == 'deal'){
-    renderDeal(card1, card2);
+    renderDeal(arg1, card2);
   } else if(action == 'player_list'){
       players.clear();
-      for (i = 1; i < list.length; i++) {
- 		players.push(list[i]);
+      for (i = 1; i < input.length; i++) {
+ 		players.push(input[i]);
       }
   } else if(action == 'join'){
-    someId = list[2];
+    someId = input[2];
+    console.log(someId+' joined');
     if(!players.includes(someId)){
-      players.push(someId);
-      updatePlayersLabel();
-      console.log(someId+' joined');
+      addPlayer(someId);
     }
-//    ping();//all players will be known to all browsers
   } else if(action == 'quit'){
-    someId = list[2];
-    temp = players[players.length]
-    for (i = 1; i < list.length; i++) {
-      if(players[i] == someId){
-        players[i] = players[players.length];
-        players.pop();
-        updatePlayersLabel();
-        console.log(someId+' quit');
-      }
-    }
+    someId = input[2];
+    removePlayer(someId);
   } else if(action == 'resetTimer'){
     seconds = 600;
-    pause = false;
+    isPaused = false;
   } else if(action == 'pause'){
-    pause = list[1];
+    isPaused = arg1;
+  } else if(action == 'clear'){
+    players = [];
+    playersLabel.change('clear');
+    quit.hide()
+    join.show()
+    watermelon.change('watermelon');
+  } else if(action == 'show'){
+    message.change(arg1+' has '+input[2]+','+input[3]);
   }
 }
 
@@ -231,7 +228,7 @@ function renderRiver(a){
 
 function turn(){
   card4 = getCard();
-  list = [];
+  var list = [];
   list[0] = 'turn';
   list[1] = card4;
   delay(send(list), 1000);
@@ -241,7 +238,7 @@ function turn(){
 
 function river(){
   card5 = getCard();
-  list = [];
+  var list = [];
   list[0] = 'river';
   list[1] = card5;
   delay(send(list), 1000);
@@ -260,14 +257,6 @@ function shuffleDeck(array) {
   }
 }
 
-function shuffle(){
-  shuffleDeck(deck);
-  list = [];
-  list[0] = 'shuffle';
-  delay(send(list), 1000);
-//  delay(send('shuffle', '', '', ''), 1000);
-  reset();
-}
 
 function reset(){
 //  tableCheck()
@@ -281,6 +270,12 @@ function reset(){
   c5.change(cardBack);
   c6.change(cardBack);
   c7.change(cardBack);
+}
+
+function shuffle(){
+  shuffleDeck(deck);
+  delay(send(['shuffle']), 1000);
+  reset();
 }
 
 function createDeck(){
@@ -328,30 +323,42 @@ function board(){
   shuff.tap = shuffle;
   createDeck();
   shuffleDeck(deck);
+  setupButtons();
 }
 
-w = stamp('watermelon',670,700)
-w.tap = joinQuit;
-join = text('join',650,800)
-quit = text('quit',650,800)
-quit.hide()
+function setupButtons(){
+  var resetButton = stamp('rtile', 600, 50, 40);
+  resetButton.tap = resetTimer;
+  var pauseButton = stamp('ptile', 650, 50, 40);
+  pauseButton.tap = pauseClock;
+  var clearButton = stamp('ctile', 700, 50, 40);
+  clearButton.tap = clearTable;
+  var showButton = stamp('stile', 750, 50, 40);
+  showButton.tap = showCards;
+  watermelon = stamp('watermelon',670,700)
+  watermelon.tap = joinQuit;
+  join = text('join',650,800)
+  quit = text('quit',650,800)
+  quit.hide()
+}
 
 function joinQuit() {
-  messageList = [];
+  var messageList = [];
   messageList[1] = Math.floor(Date.now()/1000);
-  messageList[2] = id;
+  messageList[2] = self;
   if (quit.hidden) {
     messageList[0] = 'join';
     quit.show()
     join.hide()
-    w.change('cup');
+    watermelon.change('cup');
   } else {
     messageList[0] = 'quit';
     quit.hide()
     join.show()
-    w.change('watermelon');
+    watermelon.change('watermelon');
   }
-  //  console.log('sending '+messageList);
+  console.log('sending '+messageList);
+  //sending join,1550438181,874
   send(messageList);
-
+  get(messageList);
 }
